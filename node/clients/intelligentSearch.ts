92b22c12001949b5dc5a1924fe517dc5550ca851
cnxmmora <cnxmmora@gmail.com>
2026-01-12 14:40:40 -0500

    commi

--- node/clients/intelligentSearch.ts
+++ node/clients/intelligentSearch.ts
@@ -1,42 +1,31 @@
-import { IOContext, InstanceOptions, JanusClient } from '@vtex/api'
+import { IOContext, InstanceOptions, ExternalClient } from '@vtex/api'
 import type { SearchResponse } from '../typings/search'
 
-export class IntelligentSearchClient extends JanusClient {
+export class IntelligentSearchClient extends ExternalClient {
   constructor(context: IOContext, options?: InstanceOptions) {
-    super(context, {
-      ...options,
-      headers: {
-        ...options?.headers,
-      },
-    })
-  }
-
-  public searchByCollection(collectionId: number): Promise<SearchResponse> {
-    // El formato correcto según la documentación es usar facets
-    const facets = `productClusterIds/${collectionId}`
-    
-    return this.http.get<SearchResponse>(
-      `/product_search/${facets}`,
+    super(
+      `http://${context.account}.vtexcommercestable.com.br`,
+      context,
       {
-        params: {
-          locale: 'es-CO', // opcional, ajusta según tu región
+        ...options,
+        headers: {
+          ...options?.headers,
+          'REST-Range': 'resources=0-49',
         },
-        metric: 'intelligent-search-collection',
       }
     )
   }
 
-  // Método alternativo si necesitas búsqueda sin colección
-  public search(params: any): Promise<SearchResponse> {
-    const { fullText } = params
+  public searchByCollection(collectionId: number): Promise<SearchResponse> {
+    const facets = `productClusterIds/${collectionId}`
     
     return this.http.get<SearchResponse>(
-      `/product_search/${fullText}`,
+      `/api/io/_v/api/intelligent-search/product_search/${facets}`,
       {
         params: {
           locale: 'es-CO',
         },
-        metric: 'intelligent-search',
+        metric: 'intelligent-search-collection',
       }
     )
   }
