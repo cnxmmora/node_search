299ffc0b7223d8b3625c971780e468a44d952a5e
cnxmmora <cnxmmora@gmail.com>
2026-01-12 13:05:13 -0500

    commi

--- node/index.ts
+++ node/index.ts
@@ -3,13 +3,6 @@ import { LRUCache, Service } from '@vtex/api'
 import { Clients } from './clients'
 import { intelligentSearch } from './services/intelligentSearch'
 
-type SearchArgs = {
-  fullText: string
-  from?: number
-  to?: number
-  collectionId?: string
-}
-
 const TIMEOUT_MS = 800
 
 // Crear cache LRU en memoria
@@ -44,23 +37,42 @@ declare global {
 export default new Service({
   clients,
   routes: {
-   search: async (ctx: ServiceContext<Clients>) => {
-    const body = ctx.body as Partial<SearchArgs> | undefined
-    if (!body?.fullText) {
-      ctx.status = 400
-      ctx.body = { error: 'fullText is required' }
-      return
-    }
-    const { fullText, from = 0, to = 10, collectionId } = body
-    try {
-      const result = await intelligentSearch({ fullText, from, to, collectionId }, ctx)
-      ctx.status = 200
-      ctx.body = result
-    } catch (error) {
-      console.error('Error in search route:', error)
-      ctx.status = 500
-      ctx.body = { error: 'Internal server error' }
-    }
-  }
+    search: async (ctx: ServiceContext<Clients>) => {
+      let body: any
+      
+      console.log(body,"holas_mis_Datos_2");
+      
+      try {
+        // Esto lee el body como texto y luego lo parsea
+        const text =  JSON.stringify(ctx.request)
+        body = JSON.parse(text)
+       
+        
+      } catch (err) {
+        console.error('Error parsing body', err)
+        ctx.status = 400
+        ctx.body = { error: 'Invalid JSON' }
+        return
+      }
+
+      console.log(body, 'estados_search')
+
+      if (!body?.fullText) {
+        ctx.status = 400
+        ctx.body = { error: 'fullText is required' }
+        return
+      }
+
+      const { fullText, from = 0, to = 10, collectionId } = body
+      try {
+        const result = await intelligentSearch({ fullText, from, to, collectionId }, ctx)
+        ctx.status = 200
+        ctx.body = result
+      } catch (error) {
+        console.error('Error in search route:', error)
+        ctx.status = 500
+        ctx.body = { error: 'Internal server error' }
+      }
+    },
   },
 })
